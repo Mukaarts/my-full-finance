dced3c4ae09e6d66f747ab68eba89dc3da067ca4
mukaarts <fmichael.1995@gmail.com>
2021-12-21 22:05:50 +0100

    build: add DepotFixtures

--- src/DataFixtures/DepotFixtures.php
+++ src/DataFixtures/DepotFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Depot;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,11 @@ class DepotFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $depot = (new Depot())
+            ->setTitle('Depot 1')
+        ;
 
+        $manager->persist($depot);
         $manager->flush();
     }
 }
